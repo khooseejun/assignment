2a38d91ee0a35cdf04870707aae72b1aecc356d4
Khoo See Ze <khoosz-wm25@student.tarc.edu.my>
2025-11-17 23:42:57 +0800

    Test commit

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
-# Test
-print("Hello, World!")
+from tkinter import *
+
+window = Tk()
+window.title("Python Tkinter Windows")
+window.geometry("1080x920")
+window.resizable(False,False)
+window.config(background="#0C090A")
+
+label = Label(
+    window,text="Hello, World!",
+    font=("Arial",24,"bold"),
+    fg="green",
+    bg="blue",
+    padx=10,
+    pady=10
+)
+label.pack()
+
+window.mainloop()
