b8b2c61ee41a5b4ef3dfcc33c68170986519ee37
Khoo See Ze <khoosz-wm25@student.tarc.edu.my>
2025-11-19 22:20:00 +0800

    new file:   KhooSeeZe/C_Daily Task Planner.py 	renamed:    test.py -> KhooSeeZe/test.py 	renamed:    thefile.txt -> KhooSeeZe/thetextfile.txt 	modified:   main.py 	new file:   thetextfile.txt

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 from tkinter import *
 
-class Test:
-    def __init__(self):
-        window = Tk()
-        frame1 = Frame(window, bg="blue",width=1080,height=920)
-        frame1.pack()
+window = Tk()
 
-        window.mainloop()
-
-Test()
+window.mainloop()
